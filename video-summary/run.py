7efae243b359be72231b7dec40ec1acf5c1984ab
Jason Liu <jason@jxnl.coA>
2023-02-09 15:55:06 -0500

    add summary app

--- video-summary/run.py
+++ video-summary/run.py
@@ -1,34 +1,50 @@
 from dataclasses import dataclass
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 from youtube import extract_video_id, transcribe_youtube
 from text_helpers import stream_summaries_from_text
+from logging import getLogger
 
 
-app = FastAPI()
+import modal
+
+stub = modal.Stub("summary")
+
+image = modal.Image.debian_slim().pip_install(
+    ["youtube-transcript-api", "openai", "fastapi"]
+)
+
+logger = getLogger(__name__)
 
 
 @dataclass
 class SummaryPayload:
     url: str
-    batch_size: int = 3000
-    stream: int = True
-    openai_api_key: str = None
-    engine: str = "text-davinci-003"
+    batch_size: Optional[int] = 3000
+    openai_api_key: Optional[str] = None
+    engine: Optional[str] = "text-davinci-003"
 
 
-@app.post("/stream_summary")
-async def stream_summary(req: SummaryPayload) -> StreamingResponse:
+@stub.webhook(method="POST", image=image, keep_warm=True)
+async def youtube(req: SummaryPayload) -> StreamingResponse:
     video_id = extract_video_id(req.url)
-    text = transcribe_youtube(video_id)
+    logger.info(f"Received request for {video_id}")
 
-    if len(text) == 0:
-        if req.stream:
-            return StreamingResponse(
-                status_code=404, content="Video transcript not found on youtube"
-            )
-        else:
-            return {"summary": "Video transcript not found on youtube"}
+    try:
+        text = transcribe_youtube(video_id)
+        head_text = text[:200]
+        logger.info(f"Transcript for {video_id} is {head_text}...")
+    except:
+        return StreamingResponse(
+            status_code=404, content="Video transcript not found on youtube"
+        )
+
+    if len(text) > 3000 * 10:
+        return StreamingResponse(
+            status_code=404,
+            content="Video transcript is too long to summarize without your own OpenAI API key.",
+        )
 
     # this is the generator that yields the summaries
     generator = stream_summaries_from_text(
@@ -37,11 +53,5 @@ async def stream_summary(req: SummaryPayload) -> StreamingResponse:
         openai_api_key=req.openai_api_key,
         engine=req.engine,
     )
-
-    if req.stream:
-        return StreamingResponse(generator, media_type="text/plain")
-    else:
-        acc = ""
-        async for chunk in generator:
-            acc += chunk
-        return {"summary": acc}
+    logger.info(f"Streaming summary for {video_id}...")
+    return StreamingResponse(generator, media_type="text/plain")
