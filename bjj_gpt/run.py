45759ffc437f7778aefa5db3d8dee2c2de729b01
Jason Liu <jason@jxnl.coA>
2023-02-09 10:38:42 -0500

    add index

--- bjj_gpt/run.py
+++ bjj_gpt/run.py
@@ -32,20 +32,9 @@ def transcribe(file):
 
 if __name__ == "__main__":
     files = glob.glob("data/*.mp3")
-    print(files)
 
-    transcriped_files = glob.glob("transcripts/data/**/*.txt")
-    print(transcriped_files)
-
-    # Remove files that have already been transcribed
-    files = [f for f in files if f"{f.split('/')[-1]}.txt" not in transcriped_files]
-
-    print(f"Transcribing {len(files)} files")
-
-    """
     with stub.run():
         transcriptions = transcribe.map([f"/root/{f}" for f in files])
         for fn, result in zip(files, transcriptions):
             with open("transcripts/" + fn + ".txt", "w") as f:
                 f.write(result)
-    """
