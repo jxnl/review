c0b138c58796fa78aec5637d98b5d5061697d634
Jason Liu <jason@jxnl.coA>
2023-02-12 19:22:39 -0500

    craig jones transcripts

--- bjj_gpt/run.py
+++ bjj_gpt/run.py
@@ -1,32 +1,73 @@
 import glob
 import modal
+import os
+import json
+
+from dataclasses import dataclass, field
 
 
 def download_whisper():
     import whisper
 
+    whisper.load_model("tiny")
     whisper.load_model("large")
     return
 
 
-transcribe = (
+img = (
     modal.Image.debian_slim()
     .pip_install("ffmpeg-python", "openai-whisper")
     .apt_install("ffmpeg")
     .run_function(download_whisper)
 )
 
-stub = modal.Stub(mounts=[modal.Mount(local_dir="./data", remote_dir="/root/data")])
+
+@dataclass
+class PhraseBlock:
+    # this is only to be more self documenting
+    # in case we want to support whisper
+    start: float
+    end: float
+    text: str = field(repr=False)
+    length: int = field(init=False)
+
+    def __post_init__(self):
+        self.length = len(self.text)
 
 
-@stub.function(gpu="any", image=transcribe, timeout=36000)
+def group_speech(blocks, max_chunk=30000):
+    acc = blocks[0].text
+    start = 0
+
+    for a, b in zip([b for b in blocks][:-1], [b for b in blocks][1:]):
+        # if there is a long pause
+        # or if the speech is more than 7 second
+        is_pause = (b.start - a.end) > 0.5
+        is_long = b.start - start > 6
+        is_too_long = len(acc) > max_chunk
+
+        if (is_long & is_pause) | is_too_long:
+            yield PhraseBlock(start=start, end=a.end, text=acc.strip())
+            acc = ""
+            start = b.start
+        else:
+            acc += " " + b.text
+
+
+path = "./data/craig/mp3"
+download_path = "transcripts/data/power_series/"
+
+
+stub = modal.Stub(mounts=[modal.Mount(local_dir=path, remote_dir="/root/data")])
+
+
+@stub.function(gpu="any", image=img, timeout=6000)
 def transcribe(file):
     import logging
-
-    logging.basicConfig(level=logging.INFO)
     import whisper
 
-    logging.info(f"Transcribing {file}")
+    logging.basicConfig(level=logging.INFO)
+
     model = whisper.load_model("large")
     audio = whisper.load_audio(file)
     result = model.transcribe(audio)
@@ -34,10 +75,15 @@ def transcribe(file):
 
 
 if __name__ == "__main__":
-    files = glob.glob("data/*.mp3")
+    # create the download directory if it doesn't exist
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
+
+    files = glob.glob(path + "/*.mp3")[:1]
 
     with stub.run():
-        transcriptions = transcribe.map([f"/root/{f}" for f in files])
-        for fn, result in zip(files, transcriptions):
-            with open("transcripts/" + fn + ".txt", "w") as f:
-                f.write(result)
+        file_names = [f.split("/")[-1] for f in files]
+        transcriptions = transcribe.map([f"/root/data/{f}" for f in file_names])
+        for fn, res in zip(file_names, transcriptions):
+            with open(download_path + fn + ".txt", "w") as f:
+                f.write(res)
