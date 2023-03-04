853e9e10c57229f5dc45e6bad32554f39d8245a7
Jason Liu <jason@jxnl.coA>
2023-03-03 20:51:12 -0500

    Update GPT-3.5-turbo temperature and adjust generate_followup and generate_summary functions

--- telegram-notes/app/gpt.py
+++ telegram-notes/app/gpt.py
@@ -12,22 +12,12 @@ logger = logging.getLogger(__name__)
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 
-def generate(notes, prompt: str):
-    # join all notes into a single string as a set of bullet points
-    notes = ["* " + note for note in notes]
-    notes = "\n".join(notes)
-
+def generate(messages):
     gpt3_session = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": prompt,
-            },
-            {"role": "user", "content": f"Notes:\n{notes}"},
-        ],
+        messages=messages,
         max_tokens=100,
-        temperature=0.2,
+        temperature=0.5,
     )
 
     # Generate a response to the user-provided message
@@ -39,19 +29,61 @@ def generate(notes, prompt: str):
 
 
 def generate_summary(notes: List[str]) -> str:
-    prompt = "You are an AI assistant being presented with a list of notes the I've written down throughout the day. Not all notes are related. Take those notes and generate a summary as a title. Respond only with the summary not in quotes."
-    return generate(notes, prompt)
+    system = """
+    You are an AI assistant being presented with a list of notes the I've written down throughout the day. 
+    Not all notes are related. Take those notes and generate a summary as a title. 
+    Respond only with the summary not in quotes.""".strip()
+
+    content = "\n".join([f"- {n}" for n in notes])
+
+    messages = [
+        {"role": "system", "content": system},
+        {"role": "user", "content": content},
+    ]
+    return generate(messages)
 
 
 def generate_followup(notes: List[str]) -> str:
-    prompt = "You are an AI assistant being presented with a list of notes the I've written down throughout the day. Generate some thought provoking follow up questions, only use 2-3"
-    return generate(notes, prompt)
+    system = """
+    You are a world class life coach. 
+    You are presented with notes from your clients journal. 
+    Response and try 1-2 thoughtful questions help the client explore their thoughts.
+    Do not be to percriptive, let the client lead the conversation.
+    """.strip()
+
+    if len(notes) == 1:
+        messages = [
+            {"role": "system", "content": system},
+            {"role": "user", "content": notes[0]},
+        ]
+        return generate(messages)
+
+    *rst, fst = [f"- {n}" for n in notes]
+    rst = "\n".join(rst)
+
+    messages = [
+        {"role": "system", "content": system},
+        {"role": "user", "content": rst},
+        {"role": "assistant", "content": "I'm listening..., what else happened?"},
+        {"role": "user", "content": fst},
+    ]
+
+    return generate(messages)
 
 
 if __name__ == "__main__":
     notes = [
         "I had a great day today",
         "Went ice skating with my friends",
+        "Thinking about my ice diving certification",
+        "Stressed out about work",
+        "I should work out more often to deal with the stress",
     ]
+    print("Summary:")
     print(generate_summary(notes))
+    print()
+    print("Followup:")
     print(generate_followup(notes))
+    print()
+    print("Followup only one:")
+    print(generate_followup(notes[:1]))
